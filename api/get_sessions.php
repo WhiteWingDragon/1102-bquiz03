419850e8a3d00c87f1b9c25c31660536c618ee0c
mackliu <macklun@gmail.com>
2022-01-21 16:25:20 +0800

    處理訂單後的剩餘座位及座位圖顯示判斷

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -3,8 +3,7 @@ include_once "../base.php";
 $movie=$Movie->find($_GET['id']);
 $date=$_GET['date'];
 
-
-
 for($i=1;$i<=5;$i++){
-    echo "<option value='$i'>{$ss[$i]} 剩餘座位 </option>";
+    $seats=$Ord->math('sum','qt',['movie'=>$movie['name'],'date'=>$date,'session'=>$ss[$i]]);
+    echo "<option value='$i'>{$ss[$i]} 剩餘座位 ".(20-$seats)."</option>";
 }
